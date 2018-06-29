95ad5bf13e73f7c385373c7a778a394a5b4a4b89
Sam Liebman <sammyliebman@gmail.com>
2018-06-29 12:37:32 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,1 +1,7 @@
 # define driver class here
+class Driver:
+    def passenger_names(self):
+        passenger_names = []
+        for passenger in self.passengers:
+            passenger_names.append(passenger.name)
+        return passenger_names
