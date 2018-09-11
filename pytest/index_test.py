96a27509a152a072a404a0a62b47c6317f15929d
tkoar <terrancekoar@gmail.com>
2018-09-11 15:09:59 -0400

    updated tests

--- pytest/index_test.py
+++
@@ -1,6 +0,0 @@
-import unittest2 as unittest
-from ipynb.fs.full.index import *
-
-class TestClassPlaceholder(unittest.TestCase):
-    def test_placeholder(self):
-        self.assertEqual(True, True)
